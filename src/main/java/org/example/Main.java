59498c331cad75e2ffe331135a6ccfad1cc15e7c
aleksey <meansnecessary@gmail.com>
2024-08-10 08:51:06 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketsPrice = 100;
+        int miles = ticketsPrice / 20;
+        System.out.println("bonusMiles = " + miles);
+    }
+}
+
+
+
+
+
+
+
+
+
+
